fc1f5bb50f1fc96410adc69b3c9e388be758890b
Junanjunan <taltalrealty@gmail.com>
2024-03-23 20:16:23 +0900

    wifi: [add] softAP example - Make ESP32 AP
    After setting SSID, PASS, CHANNEL, MAX_STA_CONN,
    build and you can access esp32 ap with another device

--- wifi/getting_started/softAP/main/softap_example_main.c
+++ wifi/getting_started/softAP/main/softap_example_main.c
@@ -23,10 +23,10 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
-#define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
-#define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+#define EXAMPLE_ESP_WIFI_SSID      "taltal-esp32"
+#define EXAMPLE_ESP_WIFI_PASS      "12345678" // At least 8 characters
+#define EXAMPLE_ESP_WIFI_CHANNEL   1
+#define EXAMPLE_MAX_STA_CONN       3
 
 static const char *TAG = "wifi softAP";
 
