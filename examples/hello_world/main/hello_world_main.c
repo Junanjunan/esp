cb70cb6b930c4718d9e4c1e6799d7627ec0a2cdd
Junanjunan <taltalrealty@gmail.com>
2024-03-30 18:20:23 +0900

    vTaskDelete: [add] example

--- examples/hello_world/main/hello_world_main.c
+++ examples/hello_world/main/hello_world_main.c
@@ -14,51 +14,23 @@
 #include "esp_system.h"
 
 /* handle of Task */
-TaskHandle_t xTask2Handle;
+TaskHandle_t xTaskHandle;
 
 void vTask1(void *pvParameters)
 {
-    UBaseType_t uxPriority;
-    uxPriority = uxTaskPriorityGet( NULL );
-    
-    for(;;)
+    int i = 0;
+    for (;;)
     {
-        printf("Task1 is running \r\n");
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        printf("Task1 is running and will be deleted after 5 times \r\n");
 
-        /* Setting the Task2 priority */
-        printf("About to raise the Task2 priority\r\n");
-        vTaskPrioritySet( xTask2Handle, ( uxPriority + 2) );
+        if ( i == 4 ) {
+            vTaskDelete(xTaskHandle);
+        }
 
-        // Getting the current task's name
-        const char *pcTaskName = pcTaskGetName(NULL); // NULL means the current task
-
-        // Printing the current task's name and priority
-        printf("Current Task Name 01: %s, Priority: %u\n", pcTaskName, uxPriority);
+        i++;
     }
 }
 
-void vTask2(void *pvParameters)
-{
-    UBaseType_t uxPriority;
-    uxPriority = uxTaskPriorityGet( NULL );
-    
-    for(;;)
-    {
-        printf("Task2 is running \r\n");
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-
-        /* Setting the Task2 priority */
-        printf("About to decrease the Task2 priority\r\n");
-        vTaskPrioritySet( NULL, ( uxPriority - 1) );
-
-        // Getting the current task's name
-        const char *pcTaskName = pcTaskGetName(NULL); // NULL means the current task
-
-        // Printing the current task's name and priority
-        printf("Current Task Name 02: %s, Priority: %u\n", pcTaskName, uxPriority);
-    }
-}
 
 void app_main(void)
 {
@@ -66,12 +38,11 @@ void app_main(void)
 
     printf("Hello world!\n");
 
-    xTaskCreate(vTask1, "Task 1", 2048, NULL, 4, NULL);
-    xTaskCreate(vTask2, "Task 2", 2048, NULL, 3, &xTask2Handle);
+    xTaskCreate(vTask1, "Task 1", 2048, NULL, 4, &xTaskHandle);
 
     while (1) {
         printf("print! in %d seconds...\n", i);
         i++;
-        vTaskDelay(4000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
