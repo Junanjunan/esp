695660b108bb411bc430f0228523d787c7487c3e
Junanjunan <taltalrealty@gmail.com>
2024-04-08 22:51:14 +0900

    xTimerReset: [add] lecture 03 16 executed but error occured
    E (20319) task_wdt: Task watchdog got triggered. The following tasks/users did not reset the watchdog in time:
    E (20319) task_wdt:  - IDLE0 (CPU 0)
    E (20319) task_wdt: Tasks currently running:
    E (20319) task_wdt: CPU 0: main
    E (20319) task_wdt: CPU 1: IDLE1
    E (20319) task_wdt: Print CPU 0 (current core) backtrace
    
    Backtrace: 0x400D7F57:0x3FFB0E30 0x400D831C:0x3FFB0E50 0x40082FDD:0x3FFB0E80 0x400D5559:0x3FFB4F20 0x400E41EC:0x3FFB4F60 0x40086475:0x3FFB4F90
    0x400d7f57: task_wdt_timeout_handling at /home/taltal/git/esp/esp-idf/components/esp_system/task_wdt/task_wdt.c:441
    0x400d831c: task_wdt_isr at /home/taltal/git/esp/esp-idf/components/esp_system/task_wdt/task_wdt.c:515
    0x40082fdd: _xt_lowint1 at /home/taltal/git/esp/esp-idf/components/xtensa/xtensa_vectors.S:1240
    0x400d5559: app_main at /home/taltal/git/esp/examples/hello_world/main/xQueue_03_16.c:68
    0x400e41ec: main_task at /home/taltal/git/esp/esp-idf/components/freertos/app_startup.c:208
    0x40086475: vPortTaskWrapper at /home/taltal/git/esp/esp-idf/components/freertos/FreeRTOS-Kernel/portable/xtensa/port.c:134

---
+++ examples/hello_world/main/xQueue_03_16.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#define GPIO_INPUT_USER_BUTTON      0
+#define GPIO_INPUT_BUTTON_SEL      (1ULL<<GPIO_INPUT_USER_BUTTON)
+#define ESP_INTR_FLAG_DEFAULT       0
+#define mainBACKLIGHT_TIMER_PERIOD  pdMS_TO_TICKS( 5000UL )
+
+uint8_t count_i;
+static BaseType_t xSimulatedBacklightOn = pdFALSE;
+static TimerHandle_t xBacklightTimer = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void *arg)
+{
+    count_i = (count_i + 1) % 2;
+}
+
+/* The Software timer callback functions */
+static void prvBacklightTimerCallback( TimerHandle_t xTimer )
+{
+    TickType_t xTimeNow = xTaskGetTickCount();
+
+    /* The backlight timer expired, turn the backlight off */
+    xSimulatedBacklightOn = pdFALSE;
+    /* Print the time at which the backlight was turned off */
+    printf( "Backlight turned off at time %lu\n", xTimeNow );
+}
+
+void app_main(void)
+{
+    gpio_config_t io_conf;
+
+    // interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    // bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_BUTTON_SEL;
+    // set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    // enable pull-up mode
+    io_conf.pull_up_en = 1;
+
+    gpio_config(&io_conf);
+
+    // install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_USER_BUTTON, gpio_isr_handler, (void *) GPIO_INPUT_USER_BUTTON);
+
+
+    xBacklightTimer = xTimerCreate( "BacklightTimer", mainBACKLIGHT_TIMER_PERIOD, pdFALSE, 0, prvBacklightTimerCallback );
+
+    /* Start the timer */
+    xTimerStart( xBacklightTimer, 0 );
+
+    const TickType_t xShortDelay = pdMS_TO_TICKS( 500UL );
+    TickType_t xTimeNow;
+    printf("Press a key to turn the backlight on.\n");
+
+    for (;;)
+    {
+        /* Has a key been pressed? */
+        if (count_i != 0)
+        {
+            /* Record the time at which the key press was noted */
+            xTimeNow = xTaskGetTickCount();
+            
+            /* A key has been pressed */
+            if (xSimulatedBacklightOn == pdFALSE)
+            {
+                /* The backlight is not already on, so turn it on */
+                xSimulatedBacklightOn = pdTRUE;
+                /* Print the time at which the backlight was turned on */
+                printf( "Backlight turned on at time %lu\n", xTimeNow );
+                /* Reset the timer to turn the backlight off */
+            }
+            else
+            {
+                printf("Key pressed, resetting software timer at time %lu\r\n", xTimeNow);
+                
+            }
+
+            /* Reset the timer to turn the backlight off */
+            xTimerReset( xBacklightTimer, xShortDelay );
+
+            /* Reset count_i to wait for the next press */
+            count_i = 0;
+        }
+    }
+}
