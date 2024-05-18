fff2559f1d3a839ea2174016463f778e691ddd8f
Junanjunan <taltal@DESKTOP-0GQI6CG>
2024-05-18 22:31:06 +0900

    mac-address: [add] getting mac address

--- examples/mac_address/main/mac_address.c
+++ examples/mac_address/main/mac_address.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include "esp_system.h"
+#include "esp_mac.h"
+#include "esp_log.h"
 
 void app_main(void)
 {
+    uint8_t mac[6];  // Buffer to hold MAC address
+    esp_err_t ret = esp_efuse_mac_get_default(mac);
 
+    if (ret == ESP_OK)
+    {
+        // Print the MAC address
+        ESP_LOGI("MAC", "MAC Address: %02X:%02X:%02X:%02X:%02X:%02X",
+                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    }
+    else
+    {
+        ESP_LOGE("MAC", "Failed to get MAC address");
+    }
 }
