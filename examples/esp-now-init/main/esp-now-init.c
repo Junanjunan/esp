b98b43f390267d07157fbab160f9e7a7b5ea48b1
Junanjunan <taltalrealty@gmail.com>
2024-05-19 15:17:08 +0900

    init_wifi

--- examples/esp-now-init/main/esp-now-init.c
+++ examples/esp-now-init/main/esp-now-init.c
@@ -10,8 +10,24 @@
 #include "nvs_flash.h"
 #include "esp_log.h"
 
+static const char * TAG = "esp_now_init";
+
+static esp_err_t init_wifi(void)
+{
+    wifi_init_config_t wifi_init_config =  WIFI_INIT_CONFIG_DEFAULT();
+    esp_netif_init();
+    esp_event_loop_create_default();
+    nvs_flash_init();
+    esp_wifi_init(&wifi_init_config);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_storage(WIFI_STORAGE_FLASH);
+    esp_wifi_start();
+    ESP_LOGI(TAG, "wifi init completed");
+    return ESP_OK;
+}
+
 
 void app_main(void)
 {
-
+    ESP_ERROR_CHECK(init_wifi());
 }
